c3df74e22a17be040ef1cb99281a150ab39e5265
pridelee11 <pride_lee@naver.com>
2021-11-19 16:16:31 +0900

    add webview codes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { v4: uuidV4 } = require('uuid');
+const { v4: uuidV4 } = require("uuid");
 
-router.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`);
+router.get("/", (req, res) => {
+  console.log("jayden", "render");
+  // res.redirect(`/${uuidV4()}`);
+  res.render("index");
 });
 
-router.get('/:room', (req, res) => {
-  res.render('index', { roomId: req.params.room });
+router.get("/:room", (req, res) => {
+  res.render("index", { roomId: req.params.room });
 });
 
 module.exports = router;
