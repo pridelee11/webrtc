367a9f9d70243eb9cb31bc5faf33f60e9caf7c1a
jayden.lee <jayden.lee@lge.com>
2021-11-23 19:27:05 +0900

    change to stun

--- call.js
+++ call.js
@@ -26,7 +26,7 @@ function init(userId) {
         { url: "stun:stun4.l.google.com:19302" },
         { url: "stun:global.stun.twilio.com:3478?transport=udp" },
         {
-          url: "turn:numb.viagenie.ca",
+          url: "stun:numb.viagenie.ca",
           username: "pridelee@gmail.com",
           credential: "5150as",
         },
